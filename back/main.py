3e3a86074cce72aad86ca5a3eff48c08cb20af6e
FedorovKD <FedorovKD>
2024-07-07 19:54:03 +0700

    добавил авторизацию, создание нового пользователя, создание/удаление команды, получение команд по пользователю, прикрепление пользователя к команде

--- back/main.py
+++ back/main.py
@@ -1,6 +1,26 @@
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+from routers.AuthReg import router as authReg_router
+from routers.team import router as team_router
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+app = FastAPI()
+
+# Добавляем middleware для обработки CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Здесь можно указать список допустимых источников запросов
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешаем все HTTP методы
+    allow_headers=["*"],  # Разрешаем все заголовки
+)
+
+app.include_router(
+    router=authReg_router,
+    prefix='/authReg'
+)
+
+app.include_router(
+    router=team_router,
+    prefix='/team'
+)
